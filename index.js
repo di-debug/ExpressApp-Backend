0c36aafe3158083d8c41eb0dfdc260bf52c9f936
Deepak Nandi <73755418+di-debug@users.noreply.github.com>
2023-07-08 10:43:01 +0000

    database connected with dotenv

---
+++ index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import app from "./src/app.js";
+import config from "./src/config/index.js";
+
+// IFI - Self Invoke Function
+( async () => {
+    try {
+       await mongoose.connect(config.MONGODB_URL)
+       console.log("DB CONNECTED !");
+        app.on('error', (err) => {
+            console.error("Error: ", err);
+            throw err;
+        })
+        const onListening = () => {
+            console.log(`Listening on PORT ${config.PORT}`)
+        }
+        app.listen(config.PORT, onListening);
+
+    } catch (error) {
+        console.error("Error: ", error.message)
+        throw error;
+    }
+})()
