0bb90eeb7f400bc2ca599ae0a932045c0456f3d3
Deepak Nandi <73755418+di-debug@users.noreply.github.com>
2023-07-10 09:42:33 +0000

    methods and Hooks

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles";
-
-const userName = new mongoose.Schema({
+import bcrypt from "bcryptjs"
+const userSchema = new mongoose.Schema({
     name:{
         name: String,
         required: ["true","Name is Required"],
@@ -26,4 +26,19 @@ const userName = new mongoose.Schema({
     forgotPasswordExpiry: Date
 }, {timestamps:true})
 
-export default mongoose.model("User", userName);
+// Encrypt Pass Before Saving it.
+// Note:Hooks OF MONGOOSE => We Cannot use callback func in hooks cause we need refernce of password or any schemas
+userSchema.pre("save", async function(next){
+    if (!this.isModified("password"))return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods = {
+    // Compare Password
+    comparePassword : async function(enteredPassword) {
+        return await bcrypt.compare(enteredPassword, this.password)
+    }
+}
+
+export default mongoose.model("User", userSchema);
