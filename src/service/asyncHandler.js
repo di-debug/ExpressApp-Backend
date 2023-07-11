f21d47dd7877b0b11ae140f4f36425fa5b388dba
Deepak Nandi <73755418+di-debug@users.noreply.github.com>
2023-07-11 07:34:20 +0000

    Higher Order Functions

---
+++ src/service/asyncHandler.js
@@ -0,0 +1,16 @@
+c
+// Higher Order Function
+// (fn) => is reference of a function
+
+const asyncHandler = (fn) =>async (res, req, next) => {
+    try {
+     await fn(res,req, next)   
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+export default asyncHandler;
